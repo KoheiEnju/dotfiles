537dabaa99150158f8fbee8508f6288bb559d264
hyperenju <hyperenju@gmail.com>
2022-10-26 22:21:42 +0900

    add fish

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    export PATH="$PATH:$HOME/.cargo/bin"
+    export PATH="$PATH:/usr/local/go/bin"
+    export EDITOR="/usr/local/bin/nvim"
+    export MANPAGER="/bin/sh -c \"col -b -x|nvim -R -c 'set ft=man nolist nonu noma' -\""
+
+    alias ls="lsd"
+    # la, ll is already defined in fish.
+    # lt is a function with definition
+    function lt --wraps=ls --description 'List contents of directory with tree option'
+        ls --tree $argv
+    end
+    # lla is a function with definition
+    function lla --wraps=ls --description 'List contents of directory with -la'
+        ls -la $argv
+    end
+
+    # TODO: This shoud be activated only in WSL.
+    export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+
+    # this line must be at end of the file
+    starship init fish | source
+end
+
