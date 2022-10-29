f16a8d66725c302b6b49eb8c8179e29032408925
hyperenju <hyperenju@gmail.com>
2022-10-30 03:29:39 +0900

    add some path to $PATH

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
 
     set PATH "$HOME/.cargo/bin" $PATH
     set PATH "/usr/local/go/bin" $PATH
+    set PATH "$HOME/.local/bin" $PATH
     # set PATH "/usr/local/node-v18.12.0-linux-x64/bin" $PATH
     set EDITOR "/usr/local/bin/nvim"
     set MANPAGER "/bin/sh -c \"col -b -x|nvim -R -c 'set ft=man nolist nonu noma' -\""
